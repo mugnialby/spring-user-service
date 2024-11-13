32774b3de42997951bf1505e665f34857a5d7879
Alby Al Mugni <mugnialby@gmail.com>
2024-11-13 16:04:13 +0700

    Added EnableJpaAuditing annotation

--- src/main/java/com/alby/spring_user_service/SpringUserServiceApplication.java
+++ src/main/java/com/alby/spring_user_service/SpringUserServiceApplication.java
@@ -2,8 +2,10 @@ package com.alby.spring_user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringUserServiceApplication {
 
 	public static void main(String[] args) {
